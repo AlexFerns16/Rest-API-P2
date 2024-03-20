6547c11aa78dcfc1522eefb0b2245952b8825276
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-03-20 20:39:59 +0530

    commit:6 | runserver | startapp

--- py_client/basic.py
+++ py_client/basic.py
@@ -8,8 +8,11 @@ endpoint1 = "https://httpbin.org/"
 # RestAPI HTTP request > json
 endpoint2 = "https://httpbin.org/anything"
 
+# django localhost 8000
+endpoint3 = "http://127.0.0.1:8000/"
+
 # HTTP request
-get_response = requests.get(endpoint2, json={"query":"Hello World"})   
+get_response = requests.get(endpoint3, json={"query":"Hello World"})   
 print(get_response.text)    
-print(get_response.json())
+# print(get_response.json())
 print(get_response.status_code)
