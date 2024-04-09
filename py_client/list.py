ae206c1a968ce00125537752aa88e78acead0be6
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-04-09 18:30:08 +0530

    commit:54 | Testing Search Request On The Client Side

--- py_client/list.py
+++ py_client/list.py
@@ -3,8 +3,14 @@ import requests
 # django localhost:8000/api/
 endpoint = "http://127.0.0.1:8000/api/products/list/"
 
+# endpoint for search
+# search_endpoint = "http://localhost:8000/api/search/?q=hello&public=1&tag=movies"
+
 # HTTP request
 get_response = requests.get(endpoint)
+
+# HTTP request for search
+# get_response = requests.get(search_endpoint)
 
 # gives the response in python dictionary format
 print(get_response.json())
